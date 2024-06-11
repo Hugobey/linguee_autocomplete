1dd46382b0bf07f34003f685c8c2f19c4b6841ff
Hugo Blanchart <hugo.blanchart@gmail.com>
2024-06-11 13:24:40 +0200

    logic for loading cookies and accessing the page works

--- functions/autocompletionScraper.js
+++ functions/autocompletionScraper.js
@@ -14,7 +14,7 @@ const saveCookies = async (page) => {
             } 
     });
 }
-const loadCookies = (page) => {
+const loadCookies = () => {
     return new Promise (async (resolve, reject) => {
         try {
             fs.access('cookie.json', (err) => {
@@ -45,49 +45,55 @@ const loadCookies = (page) => {
     });
 };
 
+let page, browser;
+
 exports.handler = async (event, context) => {
-
-    try {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto('https://www.linguee.fr/');        
-        // const cookies = await loadCookies(page);
-        // console.log('Are there any cookies?', cookies)
-        loadCookies(page)
-        .then(async (cookies) => {
-            if(cookies) {
-                console.log('Cookies in chained promise', cookies)
-                if(!cookies) {
-                    await page.waitForSelector('#accept-choices');
-                    await page.click('#accept-choices');
-                    await saveCookies(page);
-                    console.log('Send to saveCookie function')
-                } else {
-                    await page.setCookie(...cookies);
-                    await page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] });
-                    console.log('Cookies loaded from files')
+    return new Promise ((resolve, reject) => {
+        try {     
+            loadCookies()
+            .then(async (cookies) => {
+                console.log('Cookies in chained promise')
+                browser = await puppeteer.launch();
+                page = await browser.newPage();
+    
+                if(browser && page){
+                    if(!cookies) {
+                        await page.goto('https://www.linguee.fr/');
+                        await page.waitForSelector('#accept-choices');
+                        await page.click('#accept-choices');
+                        await saveCookies(page);
+                        console.log('Send to saveCookie function');
+                    } else {
+                        await page.setCookie(...cookies);
+                        await page.goto('https://www.linguee.fr/');   
+                        // await page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] });
+                        console.log('Cookies loaded from files')
+                    };
                 };
-            };
-        })
-
-        await page.waitForSelector('#queryinput');
-        await page.focus('#queryinput');
-        await page.type('#queryinput', 'dust');
-
-        await page.waitForSelector('.autocompletion_item.sourceIsLang2.isForeignTerm')
-
-        await page.screenshot({path: 'screenshot.png', fullPage:true})
-
-        await browser.close();
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ message: 'Sending data with success!'})
-        };
-    } catch (err) {
-        console.error('An error occured while running the script', err);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ error: err.message})
-        };
-    }
+            })
+            .then(async () => {
+                await page.waitForSelector('#queryinput');
+                await page.focus('#queryinput');
+                await page.type('#queryinput', 'dust');
+                
+                await page.waitForSelector('.autocompletion_item.sourceIsLang2.isForeignTerm')
+                await page.screenshot({path: 'screenshot.png', fullPage:true})
+                await browser.close();
+                resolve({
+                    statusCode: 200,
+                    body: JSON.stringify({ message: 'Sending data with success!'})
+                });
+            })
+            .catch((error) => {
+                console.error('Error while chaining cookies', error)
+            })
+    
+        } catch (err) {
+            console.error('An error occured while running the script', err);
+            reject({
+                statusCode: 500,
+                body: JSON.stringify({ error: err.message})
+            })
+        }
+    });
 };
